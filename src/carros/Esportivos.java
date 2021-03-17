519ca863014a47141aa76c7c8239284e12ed990e
gustavocastrow <gustavocastrocs@gmail.com>
2021-03-16 22:40:03 -0300

    Comparacao potencia

--- src/carros/Esportivos.java
+++ src/carros/Esportivos.java
@@ -10,57 +10,48 @@ public class Esportivos {
 	private double zeroCem;
 	private double kmLitro;
 
-	
+	//Metodos de acesso:
 	public String getMarca() {
 		return marca;
 	}
-
 
 	public void setMarca(String marca) {
 		this.marca = marca;
 	}
 
-
 	public String getModelo() {
 		return modelo;
 	}
-
 
 	public void setModelo(String modelo) {
 		this.modelo = modelo;
 	}
 
-
 	public int getCavalos() {
 		return cavalos;
 	}
-
 
 	public void setCavalos(int cavalos) {
 		this.cavalos = cavalos;
 	}
 
-
 	public boolean isManual() {
 		return manual;
 	}
-
 
 	public void setManual(boolean manual) {
 		this.manual = manual;
 	}
 
-
 	public int getAno() {
 		return ano;
 	}
-
 
 	public void setAno(int ano) {
 		this.ano = ano;
 	}
 	
-	
+	//Metodo construtor
 	public Esportivos(String marca, String modelo, int cavalos, boolean manual, int ano) {
 		this.marca = marca;
 		this.modelo = modelo;
@@ -70,35 +61,37 @@ public class Esportivos {
 	}
 	
 	public void andar() {
-		System.out.println("Vrum vrum vrum");
+		System.out.println("" + marca + " " + modelo + " acelerando...");
 	}
 	
 	public int potencia(int cavalo) {
-		if(cavalo > 500) {
-			System.out.println("Carro com mais de 500cv, Super Esportivo");
+		if(cavalo > 700) {
+			System.out.println("" + marca + " " + modelo + " com mais de 700cv, carro Super Esportivo ");
+		} else if (cavalo > 300 ){
+			System.out.println("" + marca + " " + modelo + " com mais de 300cv e menos de 700cv, carro Esportivo ");
 		}
-		else
-		{
-			System.out.println("Carro com menos de 300cv, Esportivo de entrada");
+		else {
+			System.out.println("" + marca + " " + modelo + " com menos de 300cv, carro Comum");
 		}
 		return cavalo;
 	}
 	
-	
 	public static void main(String[] args) {
-		Esportivos carro01 = new Esportivos("McLaren", "Senna", 1000, false, 2019);
-		carro01.setAno(2019);
-		carro01.setCavalos(1000);
-		carro01.setManual(false);
-		carro01.potencia(1000);
+		Esportivos carro01 = new Esportivos("Lamborghini", "Aventador SVJ", 770, false, 2020);
 		carro01.andar();
+		carro01.potencia(770);
 		
-		Esportivos carro02 = new Esportivos("Ferrari", "F-50", 490, true, 1990);
-		carro02.setAno(1990);
-		carro02.setCavalos(490);
-		carro02.setManual(true);
-		carro02.potencia(490);
+		System.out.println("__________________________________________________________");
+	
+		Esportivos carro02 = new Esportivos("Subaru", "WRX-STI", 310, true, 2018);
 		carro02.andar();
+		carro02.potencia(310);
+		
+		System.out.println("__________________________________________________________");
+		
+		Esportivos carro03 = new Esportivos("Honda", "Civic", 210, false, 1990);
+		carro03.andar();
+		carro03.potencia(210);
 	}
 
 }
